c5a637fb117546ec019af7624bc486748f59f7fd
Shouvik <shouvikmistry24@gmail.com>
2024-01-27 18:13:25 +0530

    DataTypes Complete

--- first.js
+++ first.js
@@ -1,2 +1,28 @@
 // Hello World
 console.log("Hello World");
+// variables
+let fullName = "Tony Stark";
+let age = 10;
+let price = 99.99;
+let x = null; 
+let y = undefined;
+let z = true;// Boolean
+let a = BigInt("123");
+let b = Symbol("Hello");
+console.log(fullName + " " + age + " " + price);
+// Var , Let and Constant
+var Age = 10;
+var Age = 20;
+var Age = 80;
+console.log(Age);
+let Price = 100;
+Price = 200;
+Price = 300;
+console.log(Price);
+const PI =  3.4;
+// const PI; Dont have initilizer 
+// const PI = 4; Error
+// PI = 4; Error
+console.log(PI);
+typeof PI;
+// Objects 
