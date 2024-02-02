0910955fa877c23996578e1aead0072490207430
Shouvik <shouvikmistry24@gmail.com>
2024-02-02 18:51:17 +0530

    Added Funtion Practice

--- Functions&Methods.js
+++ Functions&Methods.js
@@ -22,4 +22,35 @@ console.log("sum is ",sum(1,2));
     }
 
     arrowSum(1,2);
+    
+/*
+Let's Practice
+Qs. Create a function using the "function" keyword that takes a String as an argument &
+returns the number of vowels in the string.
+*/
+function countVowels(str) {
+    let count = 0;
+    for (const word of str) {
+        if (word === 'A' || word === 'E' || word === 'O' || word === 'I' || word === 'U'|| word === 'a' || word === 'e' || word === 'i' || word === 'o' || word === 'u') {
+            // console.log(word);
+            count++;
+        }
+    }
+    console.log(count);
+}
+countVowels("Shouvik");
 
+/*
+Qs. Create an arrow function to perform the same task.
+*/
+let countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === 'A' || char === 'E' || char === 'O' || char === 'I' || char === 'U'|| char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+            // console.log(char);
+            count++;
+        }
+    }
+    console.log(count);
+}
+countVow("Hello world!");
